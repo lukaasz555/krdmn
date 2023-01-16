465f920f0d94a81e4dda0e530479eb6c0196c536
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-01-16 16:53:36 +0100

    events - init

---
+++ pages/olsztyn/events/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './events.module.scss';
+const index = () => {
+	return (
+		<div className={styles.events}>
+			<h1>h1 test</h1>
+		</div>
+	);
+};
+
+export default index;
