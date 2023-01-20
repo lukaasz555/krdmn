908b63285f8420dc1448d9429ae511a8f943067d
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-01-20 14:41:31 +0100

    added refs

--- components/ketrzyn/Offer/Offer.tsx
+++ components/ketrzyn/Offer/Offer.tsx
@@ -5,7 +5,7 @@ import Gallery from '../Gallery/Gallery';
 const Offer = React.forwardRef(
 	({ offerRef }: { offerRef: React.LegacyRef<HTMLElement> }) => {
 		return (
-			<section className={styles.offer} ref={offerRef}>
+			<section className={styles.offer} ref={offerRef} id='offer'>
 				<div className={styles.offer_content}>
 					<h2 className={styles.offer_header}>oferta</h2>
 					<div className={styles.offer_desc}>
