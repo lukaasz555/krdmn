9a771e441fd105f08c065f6c80f256c293eb856e
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-01-24 19:22:02 +0100

    added options to switch

--- helpers/handleCatName.ts
+++ helpers/handleCatName.ts
@@ -12,6 +12,34 @@ export const handleCatName = (item: string) => {
 			return 'Dla dzieci';
 		case '6':
 			return 'Desery';
+		case '7':
+			return 'Pizza';
+		case '10':
+			return 'Napoje gorące';
+		case '11':
+			return 'Napoje zimne';
+		case '12':
+			return 'Piwo';
+		case '13':
+			return 'Piwo 0%';
+		case '14':
+			return 'Grzańce';
+		case '15':
+			return 'Wina białe';
+		case '16':
+			return 'Wina czerwone';
+		case '17':
+			return 'Wina musujące';
+		case '18':
+			return 'Drinki';
+		case '19':
+			return 'Wódki';
+		case '20':
+			return 'Likiery / Vermuthy';
+		case '21':
+			return 'Whisky / Whiskey / Brandy';
+		case '22':
+			return 'Gin / Rum / Tequila';
 		default:
 			return null;
 	}
