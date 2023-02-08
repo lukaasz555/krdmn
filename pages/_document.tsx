3751d851d6a0e6fce29b94e951efbaf1982144bb
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-02-08 10:59:26 +0100

    added font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,11 @@ export default function Document() {
 					href='https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&family=Montserrat:wght@200;300;400;500;600&display=swap'
 					rel='stylesheet'
 				/>
+				<link rel='preconnect' href='https://fonts.googleapis.com' />
+				<link rel='preconnect' href='https://fonts.gstatic.com' />
+				<link
+					href='https://fonts.googleapis.com/css2?family=Alatsi&display=swap'
+					rel='stylesheet'></link>
 			</Head>
 			<body>
 				<Main />
