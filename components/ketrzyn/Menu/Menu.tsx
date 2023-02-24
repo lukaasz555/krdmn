de27ecf0f2dc4578767e59e14d297c5c75fe8401
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-02-24 22:59:33 +0100

    removed refs

--- components/ketrzyn/Menu/Menu.tsx
+++ components/ketrzyn/Menu/Menu.tsx
@@ -11,7 +11,39 @@ interface MenuProps {
 	drinks: ICourse[];
 }
 
-const Menu = React.forwardRef(
+const Menu = ({ courses, drinks }: MenuProps) => {
+	const coursesCategories = Array.from(
+		new Set(courses.map((c) => c.category))
+	).sort((a, b) => Number(a) - Number(b));
+
+	const drinksCategories = Array.from(
+		new Set(drinks.map((d) => d.category))
+	).sort((a, b) => Number(a) - Number(b));
+
+	const filtered = (arr: ICourse[], item: string) => {
+		return arr.filter(({ category }) => category === item);
+	};
+
+	return (
+		<section id='menu' className={styles.menu_wrapper}>
+			<h2 className={styles.menu_header}>menu</h2>
+
+			{coursesCategories.map((item) => (
+				<MenuItem title={item} items={filtered(courses, item)} key={item} />
+			))}
+
+			{drinksCategories.map((item) => (
+				<MenuItem title={item} items={filtered(drinks, item)} key={item} />
+			))}
+			<div className={styles.greetings}>
+				<h3>Spotkajmy się w Kardamonie</h3>
+				<h4>Dorota i Jacek Moczarscy</h4>
+			</div>
+		</section>
+	);
+};
+
+/* const Menu = React.forwardRef(
 	({ courses, drinks }: MenuProps, ref: React.LegacyRef<HTMLElement>) => {
 		const coursesCategories = Array.from(
 			new Set(courses.map((c) => c.category))
@@ -43,5 +75,5 @@ const Menu = React.forwardRef(
 			</section>
 		);
 	}
-);
+); */
 export default Menu;
