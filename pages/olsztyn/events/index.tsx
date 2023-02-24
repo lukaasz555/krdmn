4e8fd31b5ba92f25712b58f5505afc7dbfc02ef0
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-02-24 16:19:53 +0100

    content changes

--- pages/olsztyn/events/index.tsx
+++ pages/olsztyn/events/index.tsx
@@ -5,6 +5,7 @@ import Layout from '../layout';
 import { slideFromBottom } from '../../../helpers/animations';
 import Image from 'next/image';
 import EventPic from '../../../public/olsztyn/4.jpeg';
+import Link from 'next/link';
 
 const Events = () => {
 	useEffect(() => {
@@ -25,30 +26,42 @@ const Events = () => {
 			<Layout>
 				<section className={styles.events_wrapper} id='events'>
 					<div className={styles.events_top}>
-						<h3>Tytuł</h3>
+						<h3>Znamy się na przyjęciach</h3>
 						<div className={styles.events_top_desc}>
 							<p>
-								Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum
-								corrupti, nobis sunt facere exercitationem excepturi ullam
-								distinctio velit cumque quidem modi deserunt minus accusantium
-								saepe rem esse libero perferendis aliquam!
+								Urodziny, chrzciny, komunie, wesela, stypy - przygotujemy dla
+								Was z wielką przyjemnością. Bo Kardamon to idealne miejsce na
+								organizację uroczystości w Olsztynie!
 							</p>
 						</div>
 						<div className={styles.events_top_desc}>
 							<p>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Deleniti inventore, numquam possimus eum similique blanditiis
-								quas delectus asperiores laudantium veniam alias nemo ex odit
-								dolores dolorem quod et sit. Veritatis?
+								Jesteśmy dużą, przestronną restauracją z własnym ogrodem, a
+								nasza sala główna pomieści nawet 110 osób.
+							</p>
+							<p>
+								Chcecie coś na wyłączność? Możliwa także rezerwacja
+								indywidualnej sali zamkniętej tylko dla Was! <br />
+								Sala bankietowa idealnie sprawdzi się na małe przyjęcie w gronie
+								15 osób. Sala klubowa pozwoli Wam zorganizować uroczystość dla
+								40 osób. Sala konferencyjna to 60 miejsc przy stołach, a 100
+								miejsc w ustawieniu kinowym.
 							</p>
 						</div>
+						<h3 style={{ marginTop: '1.5em' }}>
+							Realizujemy również cateringi
+						</h3>
 						<div className={styles.events_top_desc}>
 							<p>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Deleniti inventore, numquam possimus eum similique blanditiis
-								quas delectus asperiores laudantium veniam alias nemo ex odit
-								dolores dolorem quod et sit. Veritatis?
+								Dla pięciu, dwudziestu, stu, a nawet trzystu osób! Żadna liczba
+								nam nie straszna! Obstawiamy spotkania biznesowe i przerwy
+								kawowe. Na słodko i na słono. Wszystko wedle życzenia!
 							</p>
+							<Link
+								href={`/olsztyn/contact`}
+								className={styles.events_top_desc_link}>
+								Zadzwoń i zapytaj o ofertę!
+							</Link>
 						</div>
 					</div>
 					<div className={styles.events_bottom}>
